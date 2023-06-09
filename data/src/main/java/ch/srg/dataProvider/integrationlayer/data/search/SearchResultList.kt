415b8cffe2b638c50d8f8b8ea7d95f8587e140bf
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-06-09 15:52:51 +0200

    fsfs

--- data/src/main/java/ch/srg/dataProvider/integrationlayer/data/search/SearchResultList.kt
+++ data/src/main/java/ch/srg/dataProvider/integrationlayer/data/search/SearchResultList.kt
@@ -20,37 +20,37 @@ sealed class SearchResultList : ListResult<SearchResult>() {
 }
 
 data class SearchResultMediaList(
-        override val next: String?,
-        @SerializedName("searchResultMediaList")
-        override val data: List<SearchResult>? = null,
-        override val searchTerm: String? = null,
-        override val total: Int? = null,
-        val aggregations: MediaAggregations? = null,
-        val exactMatchTotal: Int? = null,
-        val suggestionList: List<SearchSuggestion>? = null
+    override val next: String?,
+    @SerializedName("searchResultMediaList")
+    override val data: List<SearchResult>? = null,
+    override val searchTerm: String? = null,
+    override val total: Int? = null,
+    val aggregations: MediaAggregations? = null,
+    val exactMatchTotal: Int? = null,
+    val suggestionList: List<SearchSuggestion>? = null
 ) : SearchResultList()
 
 data class SearchResultShowList(
-        override val next: String? = null,
-        override val total: Int? = null,
-        @SerializedName("searchResultShowList")
-        override val data: List<SearchResult>? = null,
-        override val searchTerm: String? = null
+    override val next: String? = null,
+    override val total: Int? = null,
+    @SerializedName("searchResultShowList")
+    override val data: List<SearchResult>? = null,
+    override val searchTerm: String? = null
 ) : SearchResultList()
 
 data class SearchResultWithMediaList(
-        override val next: String? = null,
-        override val data: List<Media>? = null,
-        val searchTerm: String? = null,
-        val total: Int? = null,
-        val aggregations: MediaAggregations? = null,
-        val exactMatchTotal: Int? = null,
-        val suggestionList: List<SearchSuggestion>? = null
+    override val next: String? = null,
+    override val data: List<Media>? = null,
+    val searchTerm: String? = null,
+    val total: Int? = null,
+    val aggregations: MediaAggregations? = null,
+    val exactMatchTotal: Int? = null,
+    val suggestionList: List<SearchSuggestion>? = null
 ) : ListResult<Media>()
 
 data class SearchResultWithShowList(
-        override val next: String? = null,
-        val total: Int? = null,
-        override val data: List<Show>? = null,
-        val searchTerm: String? = null
+    override val next: String? = null,
+    val total: Int? = null,
+    override val data: List<Show>? = null,
+    val searchTerm: String? = null
 ) : ListResult<Show>()
