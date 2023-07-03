1327b0729563814dc8502bb57994770f5df98b8a
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-07-03 08:38:41 +0200

    Fix methods naming (#12)
    - Fix search media, json parsing will not throw error.
    - Rename endpoint methods `getRadioAlphabeticalRadioShowsByChannelId` to
    `getRadioAlphabeticalShowsByChannelId`

--- data/src/main/java/ch/srg/dataProvider/integrationlayer/data/remote/MediaAggregations.kt
+++ data/src/main/java/ch/srg/dataProvider/integrationlayer/data/remote/MediaAggregations.kt
@@ -1,6 +1,7 @@
 package ch.srg.dataProvider.integrationlayer.data.remote
 
-import java.util.*
+import com.squareup.moshi.JsonClass
+import java.util.Date
 
 /**
  * Copyright (c) SRG SSR. All rights reserved.
@@ -12,17 +13,34 @@ sealed interface Bucket {
     val count: Int
 }
 
+@JsonClass(generateAdapter = true)
 data class MediaTypeBucket(val mediaType: MediaType, override val count: Int) : Bucket
+
+@JsonClass(generateAdapter = true)
 data class SubtitlesAvailableBucket(val subtitlesAvailable: Boolean, override val count: Int) : Bucket
 
+@JsonClass(generateAdapter = true)
 data class DownloadAvailableBucket(val downloadAvailable: Boolean, override val count: Int) : Bucket
+
+@JsonClass(generateAdapter = true)
 data class PlayableAbroadBucket(val playableAbroad: Boolean, override val count: Int) : Bucket
+
+@JsonClass(generateAdapter = true)
 data class QualityBucket(val quality: Quality, override val count: Int) : Bucket
+
+@JsonClass(generateAdapter = true)
 data class TopicBucket(val urn: String, val title: String, override val count: Int) : Bucket
+
+@JsonClass(generateAdapter = true)
 data class ShowBucket(val urn: String, val title: String, override val count: Int) : Bucket
+
+@JsonClass(generateAdapter = true)
 data class DurationInMinutesBucket(val duration: Long, override val count: Int) : Bucket
+
+@JsonClass(generateAdapter = true)
 data class DateBucket(val date: Date, override val count: Int) : Bucket
 
+@JsonClass(generateAdapter = true)
 data class MediaAggregations(
     val mediaTypeList: List<MediaTypeBucket>? = null,
     val subtitlesAvailableList: List<SubtitlesAvailableBucket>? = null,
