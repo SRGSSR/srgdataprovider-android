6618e5705de60bf38683500c759c4ff46f75d9bc
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-08-29 14:57:03 +0200

    Update versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,12 +1,12 @@
 object Versions {
-    const val coreKtx = "1.8.0"
-    const val lifecycle = "2.5.1"
-    const val dagger = "2.44.2"
-    const val   retrofit = "2.9.0"
+    const val coreKtx = "1.10.1"
+    const val lifecycle = "2.6.1"
+    const val dagger = "2.47"
+    const val retrofit = "2.9.0"
     const val okHttp = "4.9.1"
     const val datetime = "0.4.0"
-    const val moshi = "1.14.0"
+    const val moshi = "1.15.0"
     const val detekt = "1.22.0"
-    const val paging = "3.1.1"
+    const val paging = "3.2.0"
 
 }
