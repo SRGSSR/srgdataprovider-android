44c36ad6a6dee3263dd6f0ffce6376ed9fb576fd
Loïc Dumas <loic.dumas02@gmail.com>
2023-10-12 17:51:58 +0200

    Pull version to 0.0.4

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -5,7 +5,7 @@ object Config {
 
     const val major = 0
     const val minor = 0
-    const val patch = 3
+    const val patch = 4
     const val versionName = "$major.$minor.$patch"
 
     const val maven_group = "ch.srg.data.provider"
