7c9fd53db3da72ede7ce0751f0f051926c79fbdf
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-10-20 09:51:17 +0200

    Add show search click event

--- dataprovider-retrofit/src/main/java/ch/srg/dataProvider/integrationlayer/request/IlStatisticService.kt
+++ dataprovider-retrofit/src/main/java/ch/srg/dataProvider/integrationlayer/request/IlStatisticService.kt
@@ -2,6 +2,7 @@ package ch.srg.dataProvider.integrationlayer.request
 
 import ch.srg.dataProvider.integrationlayer.data.remote.MediaStatisticPost
 import ch.srg.dataProvider.integrationlayer.data.remote.MediaStatisticResult
+import ch.srg.dataProvider.integrationlayer.data.remote.ShowStatisticResult
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -13,15 +14,18 @@ interface IlStatisticService {
     suspend fun getMediaStatisticByUrn(@Path("urn") urn: String): MediaStatisticResult
 
     @POST("2.0/mediaStatistic/byUrn/{urn}/clicked")
-    suspend fun postMediaClicked(@Path("urn") urn: String, @Body mediaStatisticsBody: MediaStatisticPost)
+    suspend fun postMediaClicked(@Path("urn") urn: String, @Body mediaStatisticsBody: MediaStatisticPost): MediaStatisticResult
 
     @POST("2.0/mediaStatistic/byUrn/{urn}/liked")
-    suspend fun postMediaLiked(@Path("urn") urn: String, @Body mediaStatisticsBody: MediaStatisticPost)
+    suspend fun postMediaLiked(@Path("urn") urn: String, @Body mediaStatisticsBody: MediaStatisticPost): MediaStatisticResult
 
     @POST("2.0/mediaStatistic/byUrn/{urn}/shared/{service}")
     suspend fun postMediaShared(
         @Path("urn") urn: String,
         @Path("service") service: String,
         @Body body: MediaStatisticPost
-    )
+    ): MediaStatisticResult
+
+    @POST("2.0/showStatistic/byUrn/{showUrn}/searchResultClicked")
+    suspend fun postSearchResultClickedByUrn(@Path("showUrn") urn: String): ShowStatisticResult
 }
