aaeea2c610584d110c9452376614cdfd295e3bc3
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-10-20 11:56:15 +0200

    Don't encode nullable to the json (#23)

--- data/src/main/java/ch/srg/dataProvider/integrationlayer/data/DataProviderJson.kt
+++ data/src/main/java/ch/srg/dataProvider/integrationlayer/data/DataProviderJson.kt
@@ -1,9 +1,12 @@
 package ch.srg.dataProvider.integrationlayer.data
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalSerializationApi::class)
 val DataProviderJson = Json {
     ignoreUnknownKeys = true
     encodeDefaults = true
     isLenient = true
+    explicitNulls = false
 }
