22b116f2a7a55916ee00390cede55392d76b585b
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-10-20 17:44:32 +0200

    Pull version to 0.7.0

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -4,8 +4,8 @@ object Config {
     const val minSdk = 21
 
     const val major = 0
-    const val minor = 6
-    const val patch = 3
+    const val minor = 7
+    const val patch = 0
     const val versionName = "$major.$minor.$patch"
 
     const val maven_group = "ch.srg.data.provider"
