190e83cb633163038a0bb4fa42973e6fdf43e69d
Gaëtan Muller <m.gaetan89@gmail.com>
2024-05-15 09:38:22 +0200

    Remove unused KAPT plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ import io.gitlab.arturbosch.detekt.Detekt
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.android) apply false
     // https://github.com/detekt/detekt
     alias(libs.plugins.detekt)
