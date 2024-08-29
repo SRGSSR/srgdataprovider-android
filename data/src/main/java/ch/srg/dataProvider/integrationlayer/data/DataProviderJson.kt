21874c8a7c044e065466f233d75bd3e01f48085e
Gaëtan Muller <m.gaetan89@gmail.com>
2024-08-29 16:10:41 +0200

    Update KotlinX Serialization

--- data/src/main/java/ch/srg/dataProvider/integrationlayer/data/DataProviderJson.kt
+++ data/src/main/java/ch/srg/dataProvider/integrationlayer/data/DataProviderJson.kt
@@ -1,9 +1,7 @@
 package ch.srg.dataProvider.integrationlayer.data
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
-@OptIn(ExperimentalSerializationApi::class)
 val DataProviderJson = Json {
     ignoreUnknownKeys = true
     encodeDefaults = true
