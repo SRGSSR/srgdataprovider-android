45d18290130efceea459d10d87f037d385f51b24
Gaëtan Muller <m.gaetan89@gmail.com>
2024-08-30 13:19:36 +0200

    Update demo app to display request response

--- dataproviderdemo/src/main/java/ch/srgssr/dataprovider/demo/MainActivity.kt
+++ dataproviderdemo/src/main/java/ch/srgssr/dataprovider/demo/MainActivity.kt
@@ -1,7 +1,9 @@
 package ch.srgssr.dataprovider.demo
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.Lifecycle
@@ -12,12 +14,15 @@ import ch.srg.dataProvider.integrationlayer.dependencies.modules.OkHttpModule
 import ch.srg.dataProvider.integrationlayer.request.IlHost
 import ch.srg.dataProvider.integrationlayer.request.IlService
 import ch.srg.dataProvider.integrationlayer.request.parameters.Bu
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 
 class MainActivity : ComponentActivity(R.layout.activity_main) {
+
+    private lateinit var loadingProgressBar: ProgressBar
+    private lateinit var messageTextView: TextView
 
     private lateinit var okHttp: OkHttpClient
     private lateinit var ilService: IlService
@@ -27,18 +32,37 @@ class MainActivity : ComponentActivity(R.layout.activity_main) {
 
         super.onCreate(savedInstanceState)
 
+        loadingProgressBar = findViewById(R.id.loading_pb)
+        messageTextView = findViewById(R.id.message_tv)
+
         okHttp = OkHttpModule.createOkHttpClient(this)
         ilService = IlServiceModule.createIlService(okHttp, ilHost = IlHost.PROD)
 
-        val flowData = flow {
+        getTvLatestEpisodes()
+    }
+
+    private fun getTvLatestEpisodes() {
+        val tvLatestEpisodesFlow = flow {
             emit(ilService.getTvLatestEpisodes(Bu.RTS))
         }
 
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                flowData.collectLatest {
-                    Log.d("Coucou", "${it.list}")
-                }
+                tvLatestEpisodesFlow
+                    .catch { exception ->
+                        loadingProgressBar.visibility = View.GONE
+                        messageTextView.text = exception.message
+                        messageTextView.visibility = View.VISIBLE
+                    }
+                    .collect { tvLatestEpisodes ->
+                        val episodesNames = tvLatestEpisodes.list.joinToString(separator = "\n") {
+                            "${it.show?.title} - ${it.title}"
+                        }
+
+                        loadingProgressBar.visibility = View.GONE
+                        messageTextView.text = episodesNames
+                        messageTextView.visibility = View.VISIBLE
+                    }
             }
         }
     }
