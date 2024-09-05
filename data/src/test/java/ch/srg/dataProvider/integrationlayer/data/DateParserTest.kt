4a63ea8c60e27bdb7ed70ce9fc101cff1279b649
Gaëtan Muller <m.gaetan89@gmail.com>
2024-09-05 09:52:54 +0200

    Switch to `kotlin-test`

--- data/src/test/java/ch/srg/dataProvider/integrationlayer/data/DateParserTest.kt
+++ data/src/test/java/ch/srg/dataProvider/integrationlayer/data/DateParserTest.kt
@@ -1,12 +1,12 @@
 package ch.srg.dataProvider.integrationlayer.data
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert
-import org.junit.Ignore
-import org.junit.Test
 import org.junit.runner.RunWith
 import java.text.ParseException
 import java.util.Date
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 /**
  * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
@@ -18,9 +18,9 @@ class DateParserTest {
     @Test
     fun test8601() {
         val parser = ISO8601DateParser()
-        Assert.assertEquals(Date(1496126175000L), parser.parseDate("2017-05-30T08:36:15.079+02:00"))
-        Assert.assertEquals(Date(1496126175000L), parser.parseDate("2017-05-30T08:36:15+02:00"))
-        Assert.assertEquals(Date(1496126175000L), parser.parseDate("2017-05-30T06:36:15Z"))
+        assertEquals(Date(1496126175000L), parser.parseDate("2017-05-30T08:36:15.079+02:00"))
+        assertEquals(Date(1496126175000L), parser.parseDate("2017-05-30T08:36:15+02:00"))
+        assertEquals(Date(1496126175000L), parser.parseDate("2017-05-30T06:36:15Z"))
     }
 
     @Test(expected = ParseException::class)
