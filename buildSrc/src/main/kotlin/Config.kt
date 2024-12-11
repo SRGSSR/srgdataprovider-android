6ca711c447cd761b8a54cf3a75c4ed8a22c0300d
Gaëtan Muller <m.gaetan89@gmail.com>
2024-12-11 09:08:11 +0100

    Bump Data Provider to 0.11.0

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -4,8 +4,8 @@ object Config {
     const val minSdk = 21
 
     const val major = 0
-    const val minor = 10
-    const val patch = 1
+    const val minor = 11
+    const val patch = 0
     const val versionName = "$major.$minor.$patch"
 
     const val maven_group = "ch.srg.data.provider"
